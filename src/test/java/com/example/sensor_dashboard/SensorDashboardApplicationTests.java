083941bba2e6d9e1836636a01cdd33a14bf9e693
Kayo Osako <lapin.587k@gmail.com>
2025-01-13 13:05:20 +0200

    Initial commit

---
+++ src/test/java/com/example/sensor_dashboard/SensorDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sensor_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
