ada9a32a86d79d94d1137b18f7d1fc550c8404f9
Kayo Osako <lapin.587k@gmail.com>
2025-01-14 12:20:21 +0200

    Refactor ArduinoController for using WebSocketMessageBrokerConfigurer instead of WebSocketConfigurer

--- src/main/java/com/example/sensor_dashboard/controller/ArduinoController.java
+++ src/main/java/com/example/sensor_dashboard/controller/ArduinoController.java
@@ -2,6 +2,8 @@ package com.example.sensor_dashboard.controller;
 
 import com.example.sensor_dashboard.service.ArduinoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,20 @@ public class ArduinoController {
 
     @Autowired
     private ArduinoService arduinoService;
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    // WebSocket channel to send data
+    // Listen to message from the frontend (React) and trigger data retrieval.
+    @MessageMapping("/sendData")
+    public void sendArduinoData() {
+        // Fetch data from Arduino
+        String arduinoData = arduinoService.readArduinoData();
+        // Broadcast data to clients
+        // Broadcast the Arduino data to all connected WebSocket clients under /topic/arduino-data
+        messagingTemplate.convertAndSend("/topic/arduino-data", arduinoData);
+    }
 
     @GetMapping("/arduino-data")
     public String getArduinoData() {
