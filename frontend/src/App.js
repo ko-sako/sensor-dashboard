9f57a9b4e62f927e953864c455b803425003c9e2
Kayo Osako <lapin.587k@gmail.com>
2025-01-21 14:12:51 +0200

    edit App.js for second temperature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,9 @@ function App() {
         <h1>Arduino Temperature Monitor</h1>
         <TemperatureDisplay />
 
+        <h1>Arduino Second Temperature Monitor</h1>
+        <SecondTemperatureDisplay />
+
         <h1>Arduino Voltage Monitor</h1>
         <VoltageDisplay />
     </div>
